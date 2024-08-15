633736741c4b0323cb7da7379f0a0fde90ee317f
Marcos Felipe <mf7778838@gmail.com>
2024-08-15 18:00:47 -0300

    Refatora;áo no teste de AccountMapper

--- backend/src/test/java/com/fastshop/e_commerce/mappers/AccountMapperTest.java
+++ backend/src/test/java/com/fastshop/e_commerce/mappers/AccountMapperTest.java
@@ -14,6 +14,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.fastshop.e_commerce.dtos.account.AccountDTO;
 import com.fastshop.e_commerce.dtos.account.AccountSummaryDTO;
+import com.fastshop.e_commerce.dtos.address.AddressDTO;
+import com.fastshop.e_commerce.dtos.shoppingCart.ShoppingCartDTO;
 import com.fastshop.e_commerce.models.AccountBO;
 import com.fastshop.e_commerce.models.AddressBO;
 import com.fastshop.e_commerce.models.ShoppingCartBO;
@@ -26,25 +28,57 @@ public class AccountMapperTest {
     private static final Integer INDEXES = 0;
 
     @Mock
-    private UserBO userBO;
+    private UserBO mockUserBO;
 
     @Mock
-    private ShoppingCartBO shoppingCartBO;
+    private ShoppingCartBO mockShoppingCartBO;
 
     @Mock
-    private AddressBO addressBO;
+    private ShoppingCartDTO mockShoppingCartDTO;
+
+    @Mock
+    private AddressBO mockAddressBO;
+
+    @Mock
+    private AddressDTO mockAddressDTO;
 
     private AccountBO accountBO;
+
+    @Nested
+    class DtoToEntity {
+
+        @Test
+        void shouldCreateAccountBOWhenAccountDTOIsPassed() {
+            // Arrange
+            when(mockShoppingCartBO.getId()).thenReturn(ID);
+            when(mockShoppingCartDTO.getId()).thenReturn(ID);
+            when(mockUserBO.getId()).thenReturn(ID);
+
+            AccountDTO input = new AccountDTO(ID, ID, mockShoppingCartDTO, List.of(mockAddressDTO));
+
+            // Act
+            AccountBO output = AccountMapper.dtoToEntity(input, mockUserBO, mockShoppingCartBO);
+
+            // Assert
+            assertEquals(ID, output.getId());
+
+            assertEquals(input.getShoppingCart().getId(), output.getShoppingCart().getId());
+
+            assertEquals(input.getUserId(), output.getUser().getId());
+
+        }
+
+    }
 
     @Nested
     class EntityToDto {
 
         @BeforeEach
         void setup() {
-            accountBO = new AccountBO(ID, userBO, shoppingCartBO, List.of(addressBO));
-            when(userBO.getId()).thenReturn(ID);
-            when(shoppingCartBO.getId()).thenReturn(ID);
-            when(shoppingCartBO.getAccount()).thenReturn(accountBO);
+            accountBO = new AccountBO(ID, mockUserBO, mockShoppingCartBO, List.of(mockAddressBO));
+            when(mockUserBO.getId()).thenReturn(ID);
+            when(mockShoppingCartBO.getId()).thenReturn(ID);
+            when(mockShoppingCartBO.getAccount()).thenReturn(accountBO);
 
         }
 
@@ -62,8 +96,8 @@ public class AccountMapperTest {
         @Test
         void shouldCreateAccountDTOWithAddressWhenAccountBOAndAddressArePassed() {
             // Arrange
-            when(addressBO.getId()).thenReturn(ID);
-            when(addressBO.getAccount()).thenReturn(accountBO);
+            when(mockAddressBO.getId()).thenReturn(ID);
+            when(mockAddressBO.getAccount()).thenReturn(accountBO);
 
             // Act
             AccountDTO output = AccountMapper.entityToDto(accountBO, accountBO.getAddresses());
@@ -86,12 +120,12 @@ public class AccountMapperTest {
         @Test
         void shouldCreateAccountSummaryDTOWhenAccountBOIsPassed() {
             // Arrange
+            when(mockUserBO.getId()).thenReturn(ID);
+
             accountBO = new AccountBO();
             accountBO.setId(ID);
 
-            userBO = new UserBO();
-            userBO.setId(ID);
-            accountBO.setUser(userBO);
+            accountBO.setUser(mockUserBO);
 
             // Act
             AccountSummaryDTO output = AccountMapper.entityToSummaryDto(accountBO);
